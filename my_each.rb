e272362d9f527d348c97533623a7cc03d004621c
Ryan McFadden <ryanmcfadden123@gmail.com>
2020-03-26 07:18:16 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,9 +1,9 @@
 def my_each(collection)
- if block_given?
-   i = 0
-   while i < 0 
-    yield collection[i]
-    i += 1 
-    else
+  if block_given?
+    i = 0
+    while i < 0 
+      yield collection[i]
+      i += 1 
+    end
   end
 end
