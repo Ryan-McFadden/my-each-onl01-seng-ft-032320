300c9952cf5d498e370fa700714245ff00cae894
Ryan McFadden <ryanmcfadden123@gmail.com>
2020-03-26 06:59:56 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,19 +1,8 @@
-class Array
-    def my_each
-        counter = 0
-        while counter != self.size
-            # We will pass into the code block each element of the array, one by one
-            yield self[counter]
-            counter += 1
-        end
-        self # Just like the normal each method, we return self at the end
-    end
+def my_each(collection)
+  if block_given?
+    yield(collection)
 end
 
-test_array = [1, 10, 100, 1000, 10000, 100000]
-
-# This is how we specify the arguments passed to the code block
-# In this case, we decide to call it 'element', but the names are up to you
-test_array.my_each do |element|
-    puts "The test array contains the number #{element}"
+my_each([1,2,3]) do |num|
+  puts "#{num}"
 end
